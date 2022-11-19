955d6e8d3430c4d806a0f0532aab3f331ff06b5c
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-11-19 13:27:07 -0600

    Remove unneeded tests.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,18 +7,6 @@ describe('salad theory', () => {
       .contains('A food entropy calculator inspired by this work.')
       .should('have.attr', 'href', 'https://saladtheory.github.io/')
   })
-  it('should have a number of ingredients input', () => {
-    cy
-      .contains('Number of Ingredients')
-      .parent()
-      .type('5')
-  })
-  it('should have a number of unique ingredients input', () => {
-    cy
-      .contains('Number of Unique Ingredients')
-      .parent()
-      .type('2')
-  })
   it('should calculate the ingredient entropy', () => {
     cy
     .contains('Number of Ingredients')
