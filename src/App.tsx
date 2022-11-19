0770896fb06a603117cb79263eaf54ac63e1dce7
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-11-19 14:12:49 -0600

    Customize title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import IngredientEntropy from "./components/IngredientEntropy";
 import Greeting from "./components/Greeting";
+import {useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = 'Food Entropy Calculator'
+  }, [])
   return (
     <>
       <Greeting />
