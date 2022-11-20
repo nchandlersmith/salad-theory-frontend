0415077fb3c36b3f564f36866fa209daa3ee5bc1
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-11-20 09:33:51 -0600

    Remove unused import.

--- src/helpers/ingredientEntropy.ts
+++ src/helpers/ingredientEntropy.ts
@@ -1,5 +1,3 @@
-import IngredientEntropy from "../components/IngredientEntropy";
-
 export const ingredientEntropy = (numberIngredients: number, numberUniqueIngredients: number): string => {
     const entropy = Math.log(numberIngredients)/Math.log(2) * numberUniqueIngredients
     const roundedEntropy = Math.round(entropy * 100)/100 // round to 2 decimal places
